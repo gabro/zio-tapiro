0f16cb3dc558ad7281cff2f37b7edfab69b6aaf4
Gabriele Petronella <gabriele@buildo.io>
2020-03-07 12:53:50 +0100

    Disable list

--- src/main/scala/tapiroexample/school/SchoolController.scala
+++ src/main/scala/tapiroexample/school/SchoolController.scala
@@ -8,8 +8,8 @@ class query extends StaticAnnotation
 class command extends StaticAnnotation
 
 trait SchoolController[+F[_], T] {
-  @query
-  def list(): F[Either[Nothing, List[School]]]
+  // @query
+  // def list(): F[Either[Nothing, List[School]]]
 
   @query
   def read(name: String): F[Either[SchoolReadError, School]]
@@ -23,8 +23,8 @@ object SchoolController {
   def create(schoolService: SchoolService) =
     new SchoolController[Task, String] {
 
-      override def list(): Task[Either[Nothing, List[School]]] =
-        schoolService.list().map(Right(_))
+      // override def list(): Task[Either[Nothing, List[School]]] =
+      //   schoolService.list().map(Right(_))
 
       override def read(name: String): Task[Either[SchoolReadError, School]] =
         schoolService.read(name).someOrFail(SchoolReadError.NotFound).either
